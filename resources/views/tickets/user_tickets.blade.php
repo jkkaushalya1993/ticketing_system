444a95d3a24444276e656c48c5c6242fbf111c83
jkkaushalya1993 <jkkaushalya1993@gmail.com>
2021-02-25 16:07:17 +1100

    refined UI

--- resources/views/tickets/user_tickets.blade.php
+++ resources/views/tickets/user_tickets.blade.php
@@ -5,14 +5,14 @@
 
 
 @section('content')
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <h1 class="h2">Tickets summary</h1>
+    
+    </div>
 
-<div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <i class="fa fa-ticket"> My Tickets</i>
-                </div>
-                <a class="btn btn-dark" href="/tickets/addnew">New Ticket</a>
+<div class="table-responsive">
+ <a class="btn btn-dark" href="/tickets/addnew">New Ticket</a>
                 @if($tickets->isEmpty())
                         <p>You have not created any tickets yet.</p>
                          
